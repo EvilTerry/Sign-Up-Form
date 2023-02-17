ec2047dd3bb7e9c9c49edd850ecc7230980707ef
EvilTerry <91133213+EvilTerry@users.noreply.github.com>
2023-02-17 16:04:22 +0200

    add error message

--- javascript.js
+++ javascript.js
@@ -2,18 +2,21 @@ function check() {
     let btn = document.querySelector('button');
     let password = document.getElementById('password');
     let confirmPassword = document.getElementById('confirm_password');
+    let error_message = document.querySelector('.error_msg');
 
     if(confirmPassword.value == '')
         return;
     
     if( password.value == confirmPassword.value)
     {
+        error_message.innerHTML = ' ';
         password.classList.remove('error');
         confirmPassword.classList.remove('error');
         btn.disabled = false;
     }
     else
     {
+        error_message.innerHTML = '* Passwords do not match';
         password.classList.add('error');
         confirmPassword.classList.add('error');
         document.querySelector('button').disabled = true;
